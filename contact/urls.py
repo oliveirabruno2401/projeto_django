9f4fc7f6cc5c376480ec48d310646f028903319c
Bruno Oliveira <oliveiraalcantarab@gmail.com>
2026-05-13 11:24:48 -0300

    Terminei o projeto Agenda

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,19 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+    path('contact/create/', views.create, name='create'),
+
+    # user
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
+
 ]
